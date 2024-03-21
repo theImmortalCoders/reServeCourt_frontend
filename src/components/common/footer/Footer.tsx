43e48fcb0a79ae4e9efb5ffdae897e07f87d1b0a
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-03-21 20:01:11 +0100

    fix: #8 Footer layout fixed

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -4,35 +4,33 @@ import { FaYoutube } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center h-auto w-full text-mainWhite">
-      <div className="flex flex-row items-center h-auto bg-darkGreen w-full justify-evenly">
-        <span className="flex flex-col">
-          <p>Godziny otwarcia</p>
-          <p>Poniedziałek - Czwartek: 7:00 - 23:30</p>
-          <p>Piątek: 7:00 - 21:00</p>
-          <p>Sobota i Niedziela: 8:00 - 20:00</p>
-        </span>
-        <span className="flex flex-col justify-center items-center">
-          <p>RESERVE COURT RZESZÓW</p>
-          <p>35-410 Rzeszów, ul. Sportowa 51</p>
-          <p>e-mail: kontakt@reservecourt.pl</p>
-          <p>telefon: +48 999 999 999</p>
-        </span>
-        <div className="flex flex-col w-auto gap-y-2">
-          <div className="flex items-center justify-start w-full">
+    <footer className="flex flex-row w-full h-auto justify-around items-center text-mainWhite bg-darkGreen py-5 ">
+        <div className="flex flex-col justify-center w-1/4">
+          <span className="text-lg">Godziny otwarcia:</span>
+          <div className="flex font-sans">Poniedziałek - Czwartek:<span className="text-mainOrange mx-2 font-sans">7:00 - 23:30</span></div>
+          <div className="flex font-sans">Piątek: <span className="text-mainOrange mx-2 font-sans">7:00 - 21:00</span></div>
+          <div className="flex font-sans">Sobota i Niedziela: <span className="text-mainOrange mx-2 font-sans">8:00 - 20:00</span></div>
+        </div>
+        <div className="flex flex-col w-1/4 justify-start items-center ">
+          <p className="text-2xl">RESERVE COURT RZESZÓW</p>
+          <p className="font-sans">35-410 Rzeszów, ul. Sportowa 51</p>
+          <p className="font-sans">e-mail: kontakt@reservecourt.pl</p>
+          <p className="font-sans">telefon: +48 999 999 999</p>
+        </div>
+        <div className="flex flex-col w-1/4 justify-end items-end  gap-y-2 pl-24">
+          <div className="flex flex-row w-full justify-start items-start">
             <FaFacebookF className="flex justify-center mr-2 text-2xl" />
-            <p>/reserveCourtRzeszow</p>
+            <p className="font-sans">/reserveCourtRzeszow</p>
           </div>
-          <div className="flex items-center justify-start w-full">
+          <div className="flex flex-row w-full justify-start items-start">
             <FaInstagramSquare className="flex justify-center mr-2 text-2xl" />
-            <p>/reservecourt</p>
+            <p className="font-sans">/reservecourt</p>
           </div>
-          <div className="flex items-center justify-start w-full">
+          <div className="flex flex-row w-full justify-start items-start">
             <FaYoutube className="flex justify-center mr-2 text-2xl" />
-            <p>/@reservecourt</p>
+            <p className="font-sans">/@reservecourt</p>
           </div>
         </div>
-      </div>
     </footer>
   );
 }

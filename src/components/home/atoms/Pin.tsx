41fda7cf993283e31df35c02ed68bdb681eb8ea4
Paweł Buczek <pbuczek7@wp.pl>
2024-03-25 23:59:49 +0100

    fix: #11 small home page fixes

--- src/components/home/atoms/Pin.tsx
+++ src/components/home/atoms/Pin.tsx
@@ -3,10 +3,10 @@ import { PiMapPinFill } from "react-icons/pi";
 export default function Pin({ className }: { className: string }) {
   return (
     <div
-      className={`w-28 h-auto flex flex-col items-center ${className} mb-20`}
+      className={`w-24 h-auto flex flex-col items-center ${className} mb-20`}
     >
-      <PiMapPinFill className="text-9xl text-darkGreen" />
-      <div className="bg-mainOrange w-full h-28 rounded-full mt-6" />
+      <PiMapPinFill className="text-8xl text-darkGreen" />
+      <div className="bg-mainOrange w-full h-24 rounded-full mt-6" />
     </div>
   );
 }

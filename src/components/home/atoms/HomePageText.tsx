8139fed0614b6ece4a2d058255195ef04fdf7b96
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-03-27 23:12:53 +0100

    feat: #21 RWD

--- src/components/home/atoms/HomePageText.tsx
+++ src/components/home/atoms/HomePageText.tsx
@@ -7,8 +7,12 @@ export default function HomePageText({
 }) {
   return (
     <div className="flex items-end">
-      <h1 className="text-5xl font-sans">{number}</h1>
-      <p className="text-3xl ml-3 font-sans">{text}</p>
+      <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-sans">
+        {number}
+      </h1>
+      <p className="text-1x1 sm:text-lg md:text-2xl xl:text-3xl ml-3 font-sans">
+        {text}
+      </p>
     </div>
   );
 }

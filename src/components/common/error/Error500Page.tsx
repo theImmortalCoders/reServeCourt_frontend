851eadbfc81bb4d034e8a11b8d800f4694530869
Paweł Buczek <pbuczek7@wp.pl>
2024-03-29 16:07:40 +0100

    feat: #18 next's Image added, changed names and dirs

--- src/components/common/error/Error500Page.tsx
+++ src/components/common/error/Error500Page.tsx
@@ -1,8 +1,16 @@
-export default function Page () {
+import Image from "next/image"
+import Logo from "@/assets/home/reServeCourt.png"
+
+export default function Error500Page () {
     return (
         <div className="flex flex-col justify-center items-center h-screen bg-mainWhite">
             <p className="text-2xl lg:text-4xl text-darkGreen">BŁĄD 500</p>
-            <img src="/assets/common/reServeCourt.png" alt="logo" className="w-1/2 sm:w-1/4"/>
+            <Image 
+                src={Logo}
+                alt="logo"
+                className="w-1/2 sm:w-1/4"
+                priority
+            />
             <p className="text-lg lg:text-3xl text-center text-darkGreen px-12">Występują problemy techniczne</p>
             <p className="text-base lg:text-2xl text-center text-darkGreen px-12">Zapraszamy później</p>
         </div>

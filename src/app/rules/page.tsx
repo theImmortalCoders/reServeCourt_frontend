336caa78391b022b61ac51d5ce62a802e54a763a
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-03-30 18:07:30 +0100

    feat: #24 Rules page

---
+++ src/app/rules/page.tsx
@@ -0,0 +1,63 @@
+function Point ({ children }: { children: React.ReactNode }) {
+    return (
+        <h1 className="text-xl md:text-2xl text-mainOrange pt-2">{ children }</h1>
+    )
+}
+
+function List ({ children }: { children: React.ReactNode }) {
+    return (
+        <ol className="list-decimal ml-10">{ children }</ol>
+    )
+}
+
+function Rule ({ children }: { children: React.ReactNode }) {
+    return (
+        <li className="text-xs md:text-sm font-sans mt-2">{ children }</li>
+    )
+}
+
+export default function Page() {
+    return (
+        <div className="flex flex-col items-center min-h-max">
+            <div className="m-8 p-5 w-3/4 h-auto border-2 border-darkGreen rounded-2xl space-y-2">
+                <div className="flex flex-col w-full items-center md:items-start text-center">
+                    <h1 className="text-3xl md:text-4xl text-darkGreen">REGULAMIN</h1>
+                    <p className="text-xs md:text-sm text-mainBlack mt-2">REGULAMIN PORTALU <span className="text-mainGreen">ReServeCourt</span> (dostępny pod adresem www.reservecourt.pl)</p>
+                </div>
+                <Point>I. Postanowienia ogólne</Point>
+                <List>
+                    <Rule>Niniejszy regulamin dotyczy korzystania z systemu rezerwacji online kortów tenisowych dostępnego na portalu ReServeCourt.</Rule> 
+                    <Rule>Korzystanie z systemu rezerwacji oznacza akceptację postanowień niniejszego regulaminu.</Rule>
+                </List>
+                <Point>II. Rezerwacja kortu</Point>
+                <List>
+                    <Rule>Rezerwacji można dokonać poprzez wypełnienie formularza rezerwacyjnego na stronie.</Rule> 
+                    <Rule>Rezerwacja jest możliwa do 14 dni przed planowanym terminem.</Rule>
+                    <Rule>Każdy użytkownik może dokonać maksymalnie dwóch rezerwacji tygodniowo.</Rule>
+                </List>
+                <Point>III. Opłaty i płatności</Point>
+                <List>
+                    <Rule>Opłata za wynajem kortu jest ustalana zgodnie z cennikiem opublikowanym na stronie.</Rule> 
+                    <Rule>Płatność za rezerwację należy dokonać online za pomocą dostępnych metod płatności.</Rule>
+                </List>
+                <Point>IV. Odwołanie Rezerwacji</Point>
+                <List>
+                    <Rule>
+                        Użytkownik ma prawo odwołać rezerwację bez ponoszenia kosztów do 48 godzin przed planowanym terminem.
+                    </Rule> 
+                    <Rule>W przypadku późniejszego odwołania, użytkownikowi nie przysługuje zwrot kosztów rezerwacji.</Rule>
+                </List>
+                <Point>V. Zasady korzystania z kortu</Point>
+                <List>
+                    <Rule>Użytkownicy są zobowiązani do przestrzegania zasad korzystania z kortu, w tym do zachowania czystości i porządku.</Rule> 
+                    <Rule>Na kortach obowiązuje zakaz palenia i spożywania alkoholu.</Rule>
+                </List>
+                <Point>VI. Postanowienia końcowe</Point>
+                <List>
+                    <Rule>Właściciel kortów zastrzega sobie prawo do zmiany regulaminu.</Rule> 
+                    <Rule>W sprawach nieuregulowanych niniejszym regulaminem zastosowanie mają przepisy ogólne prawa.</Rule>
+                </List>
+            </div>
+        </div>
+    )
+}

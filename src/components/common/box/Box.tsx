3634e3259ace002fa5500d289df548a7af530a40
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-04-06 19:58:02 +0200

    fix: #23 cleaning

--- src/components/common/box/Box.tsx
+++ src/components/common/box/Box.tsx
@@ -7,7 +7,7 @@ export default function Box({
 }>) {
   return (
     <main
-      className={`bg-mainWhite p-7 rounded shadow-md w-64 xs:w-80 lg:w-96 ${className}`}
+      className={`bg-mainWhite p-7 my-10 rounded shadow-md w-64 xs:w-80 lg:w-96 ${className}`}
     >
       {children}
     </main>

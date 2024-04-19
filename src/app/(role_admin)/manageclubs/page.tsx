aa8cb3272800fe21687e8a6987dae746b6eb4b50
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-04-19 14:24:56 +0200

    fix: #39 wrong component

--- src/app/(role_admin)/manageclubs/page.tsx
+++ src/app/(role_admin)/manageclubs/page.tsx
@@ -2,7 +2,12 @@
 import { useState, useEffect, Dispatch, SetStateAction } from "react";
 import { useQuery } from "react-query";
 import { getAllClubs } from "@/hooks/club";
+import APIImageComponent from "@/hooks/imageAPI";
 import AddClubForm from "../../../components/manageclubs/AddClubForm";
+import DashboardContainer from "@/components/common/dashboardContainer/DashboardContainer";
+import RatingStars from "@/components/common/ratingStars/RatingStars";
+import DeleteWarning from "@/components/manageclubs/DeleteWarning";
+import { MdEdit, MdDelete } from "react-icons/md";
 
 export default function ManageClubs() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -30,8 +35,66 @@ export default function ManageClubs() {
             onClick={() => setIsOpen(true)}
             className="bg-mainGreen text-mainWhite text-xl w-fit px-4 py-2 rounded"
           >
-            Dodaj kort
+            Dodaj klub
           </button>
+          {clubsLoading || clubsError ? (
+            <div>Trwa ładowanie danych...</div>
+          ) : (
+            <div className="w-11/12 lg:w-3/5 space-y-2">
+              {clubsData &&
+                clubsData.content.map((club, index) => (
+                  <DashboardContainer
+                    key={index}
+                    onClick={() =>
+                      window.location.replace(`/managecourts/${club.id}`)
+                    }
+                    className="flex h-fit cursor-pointer"
+                  >
+                    <div className="p-4 w-[20%]">
+                      <APIImageComponent
+                        imageId={club.logo.id}
+                        type={club.logo.path}
+                      />
+                    </div>
+                    <div className="flex flex-col justify-center w-full p-4 ">
+                      <p className="text-base">{club.name}</p>
+                      <p className="text-xs font-sans mb-2">
+                        {club.description}
+                      </p>
+                      <p className="text-sm text-mainOrange">
+                        {club.location.name}
+                      </p>
+                      <span className="flex items-center space-x-8 mt-2 text-xs">
+                        <p>Liczba kortów: {club.courtsAmount}</p>
+                        <span className="flex items-center space-x-1">
+                          <p>Ocena:</p>
+                          <RatingStars rating={club.rating} />
+                        </span>
+                      </span>
+                    </div>
+                    <span className="flex justify-end items-center text-2xl space-x-2 p-4">
+                      <MdEdit className="cursor-pointer hover:text-mainGreen" />
+                      <MdDelete
+                        onClick={(e: any) => {
+                          e.stopPropagation();
+                          setDeleteWarning(true);
+                          setTempId(club.id);
+                        }}
+                        className="cursor-pointer hover:text-red-600"
+                      />
+                    </span>
+                  </DashboardContainer>
+                ))}
+            </div>
+          )}
+          {deleteWarning && (
+            <DeleteWarning
+              deleteWarning={deleteWarning}
+              setDeleteWarning={setDeleteWarning}
+              tempId={tempId}
+              setTempId={setTempId}
+            />
+          )}
         </>
       ) : (
         <AddClubForm isOpen={isOpen} setIsOpen={setIsOpen} />
--- src/app/(role_admin)/managecourts/page.tsx
+++ src/app/(role_admin)/managecourts/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import AddCourtForm from "@/components/managecourts/AddCourtForm";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 
@@ -30,66 +31,8 @@ export default function ManageCourts() {
             onClick={() => setIsOpen(true)}
             className="bg-mainGreen text-mainWhite text-xl w-fit px-4 py-2 rounded"
           >
-            Dodaj klub
+            Dodaj kort
           </button>
-          {courtsLoading || courtsError ? (
-            <div>Trwa ładowanie danych...</div>
-          ) : (
-            <div className="w-11/12 lg:w-3/5 space-y-2">
-              {courtsData &&
-                courtsData.content.map((court, index) => (
-                  <DashboardContainer
-                    key={index}
-                    onClick={() =>
-                      window.location.replace(`/managecourts/${court.id}`)
-                    }
-                    className="flex h-fit cursor-pointer"
-                  >
-                    <div className="p-4 w-[20%]">
-                      <APIImageComponent
-                        imageId={court.logo.id}
-                        type={court.logo.path}
-                      />
-                    </div>
-                    <div className="flex flex-col justify-center w-full p-4 ">
-                      <p className="text-base">{court.name}</p>
-                      <p className="text-xs font-sans mb-2">
-                        {court.description}
-                      </p>
-                      <p className="text-sm text-mainOrange">
-                        {court.location.name}
-                      </p>
-                      <span className="flex items-center space-x-8 mt-2 text-xs">
-                        <p>Liczba kortów: {court.courtsAmount}</p>
-                        <span className="flex items-center space-x-1">
-                          <p>Ocena:</p>
-                          <RatingStars rating={court.rating} />
-                        </span>
-                      </span>
-                    </div>
-                    <span className="flex justify-end items-center text-2xl space-x-2 p-4">
-                      <MdEdit className="cursor-pointer hover:text-mainGreen" />
-                      <MdDelete
-                        onClick={(e: any) => {
-                          e.stopPropagation();
-                          setDeleteWarning(true);
-                          setTempId(club.id);
-                        }}
-                        className="cursor-pointer hover:text-red-600"
-                      />
-                    </span>
-                  </DashboardContainer>
-                ))}
-            </div>
-          )}
-          {deleteWarning && (
-            <DeleteWarning
-              deleteWarning={deleteWarning}
-              setDeleteWarning={setDeleteWarning}
-              tempId={tempId}
-              setTempId={setTempId}
-            />
-          )}
         </>
       ) : (
         <AddCourtForm isOpen={isOpen} setIsOpen={setIsOpen} />

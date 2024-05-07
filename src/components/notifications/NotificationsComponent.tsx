7526555e0784eeb6bb6d4c7570c50b9947aa24b6
Paweł Buczek <pbuczek7@wp.pl>
2024-05-07 21:48:51 +0200

    feat: #40 handle animation

--- src/components/notifications/NotificationsComponent.tsx
+++ src/components/notifications/NotificationsComponent.tsx
@@ -1,6 +1,6 @@
-"use client"
-import {StompSessionProvider, useStompClient} from "react-stomp-hooks";
-import {useState, useEffect} from "react";
+"use client";
+import { StompSessionProvider, useStompClient } from "react-stomp-hooks";
+import { useState, useEffect } from "react";
 import { NEXT_PUBLIC_WEBSOCKET_URL } from "@/utils/appWebsocket";
 import { markNotificationAsRead } from "@/hooks/notification";
 
@@ -12,9 +12,9 @@ export interface Message {
 }
 
 const Notifications = () => {
-    
     const [messages, setMessages] = useState<Message[]>([]);
     const stompClient = useStompClient();
+    const [removedNotifications, setRemovedNotifications] = useState<number[]>([]);
 
     useEffect(() => {
         if (stompClient) {
@@ -23,7 +23,7 @@ const Notifications = () => {
                 setMessages(prevMessages => [...prevMessages, ...parsedMessage]);
             });
 
-            stompClient.publish({destination: '/app/broadcast', body: "0"});
+            stompClient.publish({ destination: '/app/broadcast', body: "0" });
 
             return () => {
                 subscription.unsubscribe();
@@ -32,11 +32,15 @@ const Notifications = () => {
     }, [stompClient]);
 
     async function markNotification(notificationId: number) {
-        try{
+        try {
             const result = await markNotificationAsRead(notificationId);
-            if(result === 200) {
+            if (result === 200) {
                 console.log("Przeczytano powiadomienie");
-                setMessages(messages.filter(message => message.id !== notificationId));
+                setRemovedNotifications(prev => [...prev, notificationId]);
+                setTimeout(() => {
+                    setMessages(messages.filter(message => message.id !== notificationId));
+                    setRemovedNotifications(prev => prev.filter(id => id !== notificationId));
+                }, 300);
             } else {
                 console.error("Błąd odznaczania powiadomienia");
             }
@@ -47,8 +51,8 @@ const Notifications = () => {
 
     return (
         <div className="fixed bottom-0 right-0 m-6 space-y-2 min-w-48 max-w-80 text-wrap text-sm xs:text-base">
-            {messages.map((message, index) => (
-                <div key={index} className="flex flex-col justify-center items-center p-4 bg-white rounded shadow-md shadow-lightGrey transition-all duration-75">
+            {messages.map((message) => (
+                <div key={message.id} className={`flex flex-col justify-center items-center p-4 bg-white rounded shadow-md shadow-lightGrey transition-all duration-300 ${removedNotifications.includes(message.id) ? 'opacity-0 transform scale-0.9' : ''}`}>
                     <p className="mb-2 text-center">{message.message}</p>
                     <div onClick={() => markNotification(message.id)} className="bg-mainOrange cursor-pointer text-center border-[1px] border-mainOrange text-mainWhite rounded-full h-8 max-w-24 sm:max-w-32 md:max-w-48 w-full flex items-center justify-center">
                         <p>OK</p>
@@ -62,7 +66,7 @@ const Notifications = () => {
 export default function NotificationComponent() {
     return (
         <StompSessionProvider url={`${NEXT_PUBLIC_WEBSOCKET_URL}`}>
-            <Notifications/>
+            <Notifications />
         </StompSessionProvider>
     );
 }

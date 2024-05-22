ea440bcd7f49c2870418f9ddcce3587295e29204
Bartłomiej Krówka <157894166+bkrowka@users.noreply.github.com>
2024-05-22 23:34:15 +0200

    refactor: #69 Division into components

--- src/app/(auth)/my-reservations/page.tsx
+++ src/app/(auth)/my-reservations/page.tsx
@@ -1,49 +1,9 @@
 "use client"
-import { useState, useEffect, Dispatch, SetStateAction } from "react";
-import { getAllReservationByCurrentUser, cancelReservation } from "@/hooks/reservation";
+import { useState, Dispatch, SetStateAction } from "react";
+import { getAllReservationByCurrentUser, cancelReservation, ReservationData } from "@/hooks/reservation";
 import { useQuery } from 'react-query';
-import DashboardContainer from "@/components/common/dashboardContainer/DashboardContainer";
-import APIImageComponent from "@/hooks/imageAPI";
-import { MdCancel } from "react-icons/md";
-
-function CancelWarning({
-    setCancelWarning,
-    tempId,
-    handleCancelReservation
-  }: {
-    setCancelWarning: Dispatch<SetStateAction<boolean>>;
-    tempId: number;
-    handleCancelReservation: (id:number) => void;
-  }) {
-    return (
-      <div className="fixed flex items-center justify-center inset-0 z-10">
-        <div className="absolute inset-0 bg-mainWhite opacity-80"></div>
-        <div className="flex flex-col justify-center items-center w-64 sm:w-96 border-2 border-darkGreen bg-mainWhite rounded space-y-2 p-4 z-20">
-          <h1 className="text-xl">Anulowanie rezerwacji</h1>
-          <p className="text-sm text-center font-sans">
-            Czy na pewno chcesz anulować wybraną rezerwację?
-          </p>
-          <span className="space-x-4">
-            <button
-              onClick={() => setCancelWarning(false)}
-              className="text-right text-mainOrange text-sm sm:text-md"
-            >
-              Anuluj
-            </button>
-            <button
-              onClick={() => {
-                handleCancelReservation(tempId);
-                setCancelWarning(false);
-            }}
-              className="bg-red-600 text-mainWhite text-sm rounded px-4 py-2 w-fit"
-            >
-              Tak
-            </button>
-          </span>
-        </div>
-      </div>
-    );
-  }
+import ReservationListComponent from "@/components/my-reservations/ReservationListComponent";
+import CancelWarning from "@/components/my-reservations/CancelWarning";
 
 const HistoryCurrentSwitch = ({
     isCurrent,
@@ -75,7 +35,6 @@ const HistoryCurrentSwitch = ({
         </div>
     )
 }
-
 
 export default function MyReservationsPage() {
 
@@ -113,37 +72,12 @@ export default function MyReservationsPage() {
                     Array.isArray(reservationsData) &&
                     reservationsData.map((reservation) => (
                         !reservation.canceled && (
-                            <DashboardContainer key={reservation.id} className="flex flex-col md:flex-row md:h-36">
-                                <div className="flex items-center w-40 p-4">
-                                    <APIImageComponent imageId={reservation.court.image.id} type={"type"} />
-                                </div>
-                                <div className="flex flex-col justify-start h-full w-full p-4">
-                                    <p className="text-base">{reservation.court.name}</p>
-                                    <p className="text-sm text-mainOrange text-wrap">
-                                        {reservation.court.location.name}
-                                    </p>
-                                    <div className="hidden md:flex flex-col w-full md:h-full justify-end min-h-12">
-                                        <p className="text-sm font-bold">Zarezerwowano:</p>
-                                        <p className="text-xs font-sans text-nowrap">
-                                            {reservation.timeFrom.slice(0, -3).replace('T', ' ')} : {reservation.timeTo.slice(11, -3).replace('T', ' ')}
-                                        </p>
-                                    </div>
-                                </div>
-                                <span className="flex justify-end items-center p-4">
-                                    <div className="flex md:hidden flex-col justify-end space-y-1 h-full w-full">
-                                        <p className="text-sm font-bold">Zarezerwowano:</p>
-                                        <p className="text-xs font-sans text-nowrap">
-                                            {reservation.timeFrom.slice(0, -3).replace('T', ' ')} : {reservation.timeTo.slice(11, -3).replace('T', ' ')}
-                                        </p> 
-                                    </div>
-                                    <span className="flex space-x-3 md:space-x-2 text-3xl md:text-2xl">
-                                        <MdCancel onClick={() => {
-                                            setTempId(reservation.id);
-                                            setCancelWarning(true);
-                                        }} className="cursor-pointer hover:text-red-600"/>
-                                    </span>
-                                </span>
-                            </DashboardContainer>
+                            <ReservationListComponent 
+                                key={reservation.id} 
+                                reservation={reservation} 
+                                setCancelWarning={setCancelWarning} 
+                                setTempId={setTempId}
+                            />
                         )
                     ))
                 )

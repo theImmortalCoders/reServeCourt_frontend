96d78974ce5a9ba803cf3ae4e5614f022f09100e
Marcin Bator <marcinbator.ofc@gmail.com>
2024-06-16 14:10:36 +0200

    map fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
           integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
           crossOrigin=""
         />
-
         <script
           src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
           integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
--- src/components/home/organisms/Map.tsx
+++ src/components/home/organisms/Map.tsx
@@ -1,13 +1,36 @@
 "use client";
 import { getAllClubs } from "@/hooks/club";
-import React from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
+import dynamic from "next/dynamic";
+
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
+);
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
+  ssr: false,
+});
 
 export default function Map() {
   const { data: clubsData } = useQuery("clubs", getAllClubs);
+  const [isClient, setIsClient] = useState(false);
 
-  if (clubsData?.content.length === 0 || clubsData === undefined) return <></>;
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient || clubsData?.content.length === 0 || clubsData === undefined)
+    return <></>;
+
   return (
     <div>
       <h2 className="w-full text-center py-10 uppercase text-2xl sm:text-lg md:text-2xl lg:text-3xl xl:text-4xl text-darkGreen">
@@ -26,7 +49,10 @@ export default function Map() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {clubsData?.content.map((club) => (
-          <Marker position={[club.location.locX, club.location.locY]}>
+          <Marker
+            key={club.id}
+            position={[club.location.locX, club.location.locY]}
+          >
             <Popup>
               <a href={`/clubs/${club.id}`}>{club.name}</a>
             </Popup>
